a14276de4080a13369b02f342111a4807df6f10f
KT <132851569+kumih0@users.noreply.github.com>
2023-06-30 23:42:36 -0500

    bulk create blogpost and comments

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,5 +1,6 @@
 const sequelize = require('../config/connection');
 const userData = require('./userData.json');
+const blogpostData = require('./blogData.json');
 const { User, BlogPost, Comment } = require('../models');
 
 const seedDatabase = async () => {
@@ -9,6 +10,10 @@ const seedDatabase = async () => {
         individualHooks: true,
         returning: true,
     });
+
+    await BlogPost.bulkCreate(blogpostData, {include: {model: User}});
+
+    await Comment.bulkCreate(commentData, {include: {model: User, BlogPost}});
 
     process.exit(0);
     }
