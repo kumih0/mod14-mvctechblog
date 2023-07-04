f040bec2aafdcf0639aef656b2c73a5d6559ff61
KT <132851569+kumih0@users.noreply.github.com>
2023-07-04 09:05:13 -0500

    new user and login route for user api route

---
+++ controller/api/userRoutes.js
@@ -0,0 +1,47 @@
+const router = require('express').Router();
+const { User, BlogPost, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+//new user sequence
+router.post('/', async (req, res) => {
+    try{
+        const userData = await User.create(req.body);
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+            res.status(200).json(userData);
+            console.log('new user created', userData);
+        });
+    } catch(err){
+        res.status(400).json(err);
+        console.log('u broke it stupid', err);
+    }
+});
+
+//login route
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await User.findOne({ where: { email: req.body.email }});
+        if (!userData) {
+            res.status(400).json({ message: 'Incorrect email or password, please try again' });
+            return;
+        }
+        const validPassword = await userData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect email or password, please try again' });
+            return;
+        }
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+            console.log('logged in', userData);
+            res.json({ user: userData, message: 'You are now logged in!' });
+        });
+    } catch (err) {
+        res.status(400).json(err);
+        console.log('wtf', err);
+    }
+});
+
+module.exports = router;
