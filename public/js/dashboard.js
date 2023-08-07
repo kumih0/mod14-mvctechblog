f825309c2438d2fc0fadfc3a2b901b97db65a7fb
KT <132851569+kumih0@users.noreply.github.com>
2023-08-07 15:40:20 -0500

    corrected routes for dashboard and making posts

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -21,7 +21,7 @@ const newBlogpostHandler = async (event) => {
 };
 
 const deleteBlogpostHandler = async (event) => {
-    if(event.target.hasAttribute('data-delId')) {
+    if (event.target.hasAttribute('data-delId')) {
         const id = event.target.getAttribute('data-delId');
 
         const response = await fetch(`/api/blogposts/${id}`, {
@@ -38,9 +38,43 @@ const deleteBlogpostHandler = async (event) => {
 };
 
 
-const editBlogpostHandler = async (event) => {
-    if(event.target.hasAttribute('data-editId')) {
+const editBlogpostRender = async (event) => {
+    if (event.target.hasAttribute('data-editId')) {
         const id = event.target.getAttribute('data-editId');
 
+        const blogpostData = await BlogPost.findOne({
+            where: {
+                id: id,
+            },
+        });
+
+        if (blogpostData) {
+            document.querySelector('#form-header').textContent = 'Edit Blogpost';
+            document.querySelector('#blogpost-title').value = blogpostData.title;
+            document.querySelector('#blogpost-content').value = blogpostData.content;
+
+            document.querySelector('#post-btn').textContent = 'Save Changes';
+        }
+
+        document.querySelector('#post-btn').addEventListener('click', editBlogpostHandler);
 
     }
+};
+
+const editBlogpostHandler = async (event) => {
+    const title = document.querySelector('#blogpost-title').value.trim();
+    const content = document.querySelector('#blogpost-content').value.trim();
+
+    const response = await fetch(`/api/blogposts/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({ title, content }),
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    }
+    else {
+        alert(response.statusText);
+    }
+};
