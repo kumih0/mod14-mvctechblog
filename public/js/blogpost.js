2a6c923f4cbeab8b01087ed5f1e75c76d9cd5f1e
KT <132851569+kumih0@users.noreply.github.com>
2023-08-07 17:41:33 -0500

    adjusting template and blogpost routes

--- public/js/blogpost.js
+++ public/js/blogpost.js
@@ -1,1 +1,22 @@
-const 
+const newCommentHandler = async (event) => {
+    event.preventDefault();
+    const content = document.querySelector('#comment-content').value.trim();
+    const blogpost_id = document.querySelector('#blogpost-id').value.trim();
+
+    if (content) {
+        const response = await fetch('/api/comments', {
+            method: 'POST',
+            body: JSON.stringify({ content, blogpost_id }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace(`/blogpost/${blogpost_id}`);
+        }
+        else {
+            alert(response.statusText);
+        }
+    }
+}
+
+document.querySelector('#comment-form').addEventListener('submit', newCommentHandler);
